39ee2ed27bb58f325eb177e1c4acd9c32eca812d
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-19 15:56:09 -0300

    Update p_queue.c

--- make_huffman/p_queue.c
+++ make_huffman/p_queue.c
@@ -7,22 +7,19 @@ pqueue *createPriorityQueue()
   return new;
 }
 
-void priorityEnqueue(pqueue* priorityQueue, huffmanTree *newTreeNode)
-{
+void priorityEnqueue(pqueue *priorityQueue, huffmanTree *newTreeNode){
 
   node * new = (node*) malloc(sizeof(node));
   new->item = newTreeNode;
 
-  if(priorityQueue->head == NULL || ((huffmanTree*)new->item)->freq <= ((huffmanTree*)((node*)priorityQueue->head)->item)->freq)
-  {
+  if(priorityQueue->head == NULL || ((huffmanTree*)new->item)->freq <= ((huffmanTree*)priorityQueue->head->item)->freq){
     new->next = priorityQueue->head;
     priorityQueue->head = new;
   }
-  else
-  {
+  else{
     node * current = priorityQueue->head;
 
-    while(current->next != NULL && ((huffmanTree*)((node*)current->next)->item)->freq < ((huffmanTree*)new->item)->freq){
+    while(current->next != NULL && ((huffmanTree*)current->next->item)->freq < ((huffmanTree*)new->item)->freq){
       current = current->next;
     }
 
@@ -31,17 +28,15 @@ void priorityEnqueue(pqueue* priorityQueue, huffmanTree *newTreeNode)
   } 
 }
 
-huffmanTree * priorityDequeue(pqueue *priorityQueue)
-{
+huffmanTree * priorityDequeue(pqueue *priorityQueue){
 
   if(priorityQueue->head == NULL){
     printf("PRIORITY QUEUE UNDERFLOW!\n");
     return NULL;
   }
-  else
-  {
+  else{
     node * auxiliar = priorityQueue->head;
-    priorityQueue->head = ((node*)priorityQueue->head)->next;
+    priorityQueue->head = priorityQueue->head->next;
     auxiliar->next = NULL;
     huffmanTree * returnNode = (huffmanTree*)auxiliar->item;
     free(auxiliar);
