88c99c4585b455151ae28dd21d55535f524cd0c8
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-23 19:49:42 -0300

    Update hash.h

--- make_huffman/hash.h
+++ make_huffman/hash.h
@@ -10,6 +10,9 @@
 
 #define ASCII_SIZE 256
 
+/*
+Structures of a hash and a hash element.
+*/
 typedef struct hash_table hash;
 typedef struct hash_element helement;
 
@@ -22,10 +25,30 @@ struct hash_element{
   long int characterFrequency;
 };
 
+/*
+This function creates a hash table by allocating
+the corresponding structures above. The binary code
+from the hash element will be allocated with the
+maximum size indicated. Then it returns the hash pointer.
+*/
 hash *createHash(int maximumSize);
 
+/*
+This function builds a new codification for the
+characters that are in the file. It does that by
+recursively navigating in the Huffman Tree and
+handling a string. It puts a 0 in the string's current
+position when going to the left node's child or a 1
+when going to the right child. When it arrives in a leaf
+the string formed is copied to the corresponding
+element in the hash table and the recursion stops.
+*/
 void buildHash(hash *hashTable, huffmanTree *huffmanNode, int position, unsigned char *auxiliarString);
 
+/*
+This functions frees the memory allocated
+to avoid memory leak.
+*/
 void eraseHash(hash * hashTable);
 
 #endif
