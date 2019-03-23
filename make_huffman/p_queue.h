e379dad0de5a6b65053ffbbdb23ec642538e0c90
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-23 20:04:52 -0300

    Update p_queue.h

--- make_huffman/p_queue.h
+++ make_huffman/p_queue.h
@@ -8,6 +8,10 @@
 #include <ctype.h>
 #include "tree.h"
 
+/*
+Structures of a priority queue and a
+linked list node.
+*/
 typedef struct priority_queue pqueue;
 typedef struct list_adt node;
 
@@ -22,11 +26,27 @@ struct list_adt
   node * next;
 };
 
-
+/*
+This function creates a priority queue by
+allocating the corresponding structures.
+Then it returns the queue pointer.
+*/
 pqueue *createPriorityQueue();
 
+/*
+This function adds a Huffman Tree node in
+the priority queue. It navigates in the queue
+until the current queue element has a bigger
+frequency then the one that is going to be
+added and adds the element in that position.
+*/
 void priorityEnqueue(pqueue* priorityQueue, huffmanTree *newTreeNode);
 
+/*
+This function dequeues the first element
+of the queue and returns the Huffman Tree
+node pointer from the dequeued element.
+*/
 huffmanTree * priorityDequeue(pqueue* priorityQueue);
 
 #endif
