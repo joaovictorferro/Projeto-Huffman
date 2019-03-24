de18c58b1e4861e6a788d68ece0da2651099c04f
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-23 21:49:06 -0300

    Update decompress.h

--- make_huffman/decompress.h
+++ make_huffman/decompress.h
@@ -8,7 +8,9 @@
 #include <ctype.h>
 #include "utilities.h"
 
-/* structure of a tree node */
+/*
+Structure of a tree node.
+*/
 typedef struct node_tree node_t;
 
 struct node_tree 
@@ -18,43 +20,74 @@ struct node_tree
 	node_t  *right;
 };
 
-/* create a tree with the character and left and right trees, respectively, 
- * and returns the address for the tree created */
+/* 
+This function creates a tree with the character
+and left and right trees, respectively, and
+returns the pointer for the tree created.
+*/
 node_t *create_node(unsigned int character, node_t *left, node_t *right);
 
-/* frees memory occupied by all nodes in the tree */
+/*
+This function frees memory allocated to avoid
+memory leak.
+*/
 void free_tree(node_t *huff_tree);
 
-/* returns TRUE if the received node is a sheet and FALSE otherwise */
+/*
+This funciotn identifies if a node is a leaf
+and returns 1 for positive and 0 for a negstive
+result. 
+*/
 int is_leaf_decompress(node_t *binary_tree);
 
-/* returns TRUE if the byte bit is set to the received position and FALSE
- * otherwise */
+/*
+This function identifies if a bit from a given
+byte position is set and returns 1 for positive
+or 0 for a negative result.
+*/
 int is_bit_set_decompress(unsigned int character, int position); 
 
-/* read byte the byte of the file after the tree and write the corresponding 
- * characters according to the received huffman tree */
+/*
+This function reads the file byte by byte starting
+from the second byte. Then it writes the corresponding 
+characters according to the received huffman tree in
+output file.
+*/
 void decom(FILE *input_file, unsigned int trash_size, int tree_size, node_t *huff_tree, FILE *output_file);
 
-/* returns the amount of garbage bits that is written to the first byte of the
- * received in the first 3 bits */
+/*
+This function gets the trash size information from the
+first file's byte and returns it.
+*/
 unsigned int get_trash_size(FILE *input_file); 
 
-/* returns the size of the tree that is written in the 13 bits next to the trash */
+/*
+This function gets the tree size information from the
+first two file's bytes and returns it.
+*/
 unsigned int get_tree_size(FILE *input_file); 
 
-/* returns an array with the bytes of the tree written in the file, after the two
- * first bytes, according to the size of the tree received */
+/*
+This function gets the tree pre-order string starting
+from the third file's byte and returns it.
+*/
 unsigned int *get_tree_array(FILE *input_file, unsigned int tree_size);
 
-/* creates a tree that is received written in preorder in an array and returns */
+/*This funtion recursively builds the Huffmen Tree that
+is received written in pre-order in the file and returns
+the root's pointer.
+*/
 node_t *build_tree(unsigned int **tree_array); 
 
-/* returns a tree written in preorder in the tree_size bytes after the second
- * byte in file received */
+/*
+This function handles the tree building process and
+returns the root's pointer.
+*/
 node_t *get_tree(FILE *input_file, unsigned int tree_size);
 
-/* main to initialize decompress */
+/*
+This function handles the hole decompress process. 
+*/
 void decompressMain();
 
 #endif
