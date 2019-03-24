f45580a926fd81670390f2dfcd8564cf8bc88cdf
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-23 21:28:43 -0300

    Update utilities.h

--- make_huffman/utilities.h
+++ make_huffman/utilities.h
@@ -7,6 +7,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-FILE *OpenFile();
+/*
+This function reads the name of
+the file given by the user and
+opens it in binary read mode. The
+/0 is put in the last string position
+to exclude the /n character. 
+*/
+FILE *OpenFile(char name[]);
 
 #endif
