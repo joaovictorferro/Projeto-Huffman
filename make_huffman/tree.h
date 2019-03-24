74b0ca6932d8e74a5b415041c2fc148ed3453f60
lucasmmassa <43853737+lucasmmassa@users.noreply.github.com>
2019-03-23 21:17:44 -0300

    Update tree.h

--- make_huffman/tree.h
+++ make_huffman/tree.h
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/*
+Structure from
+*/
 typedef struct huffmanTree huffmanTree;
 
 struct huffmanTree
@@ -16,16 +19,42 @@ struct huffmanTree
   huffmanTree * left, *right;
 };
 
+/*
+This function returns the biggest
+value that was passed in the arguments.
+*/
 int max(int a, int b);
 
+/*
+This function recursively calculates
+and returns the height of a tree node.
+*/
 int height(huffmanTree *tree);
 
+/*
+This function identifies if the node
+passed as an argument is a leaf and
+returns 1 for a positive and 0 for a
+negative answer.
+*/
 int isLeaf(huffmanTree *treeNode);
 
+/*
+This function prints the tree in
+pre-order in the terminal.
+*/
 void printTree(huffmanTree *tree);
 
+/*
+This function frees the allocated
+memory to avoid memory leak.
+*/
 void eraseTree(huffmanTree *tree);
 
+/*
+This function creates a new Huffman
+Tree node and returns its pointer.
+*/
 huffmanTree *newNode(unsigned char c, long int f);
 
 #endif
